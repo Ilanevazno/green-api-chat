fb0075c8115e1f261eecb815c1f73b4ace4e18f8
Сигидин Илья <ilya.sigidin@gazprombank.ru>
2023-05-17 06:08:12 +0300

    removed mocks

--- src/api/contacts/index.ts
+++ src/api/contacts/index.ts
@@ -1,14 +1,11 @@
 import axiosInstance from "config/axios";
 import { IContact, IReceiveNotification, ISendMessage } from "entities/contact";
 
-window.idInstance = "1101820955";
-window.apiTokenInstance = "cc7850ea1dd64c5ba26ce33e4ae5a2a789f512a1fd334561bf";
-
 export const getContact = async (chatId: string): Promise<IContact> => {
   const { idInstance, apiTokenInstance } = window;
 
   const response = await axiosInstance.post(
-    `https://api.green-api.com/waInstance${idInstance}/getContactInfo/${apiTokenInstance}`,
+    `waInstance${idInstance}/getContactInfo/${apiTokenInstance}`,
     {
       chatId: `${chatId}@c.us`,
     }
@@ -24,7 +21,7 @@ export const sendMessage = async ({
   const { idInstance, apiTokenInstance } = window;
 
   const response = await axiosInstance.post(
-    `https://api.green-api.com/waInstance${idInstance}/sendMessage/${apiTokenInstance}`,
+    `waInstance${idInstance}/sendMessage/${apiTokenInstance}`,
     {
       chatId,
       message,
@@ -38,7 +35,7 @@ export const receiveNotification = async (): Promise<IReceiveNotification> => {
   const { idInstance, apiTokenInstance } = window;
 
   const response = await axiosInstance.get(
-    `https://api.green-api.com/waInstance${idInstance}/receiveNotification/${apiTokenInstance}`
+    `waInstance${idInstance}/receiveNotification/${apiTokenInstance}`
   );
 
   return response.data;
@@ -50,7 +47,7 @@ export const deleteNotification = async (
   const { idInstance, apiTokenInstance } = window;
 
   const response = await axiosInstance.delete(
-    `https://api.green-api.com/waInstance${idInstance}/deleteNotification/${apiTokenInstance}/${id}`
+    `waInstance${idInstance}/deleteNotification/${apiTokenInstance}/${id}`
   );
 
   return response.data;
--- src/store/auth/slice.ts
+++ src/store/auth/slice.ts
@@ -1,37 +1,14 @@
 import { createSlice } from "@reduxjs/toolkit";
-// import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-// import { api } from "api";
-// import { ICharactersRequest } from "api/auth/interfaces";
-// import { IPagination } from "api/interfaces";
-// import { ICharacter } from "entities/characters";
 
-interface LandingPageState {
+interface IAuthState {
   idInstance?: string;
   apiTokenInstance?: string;
   isAuthenticated: boolean;
 }
 
-const initialState: LandingPageState = {
-  // idInstance: undefined,
-  // apiTokenInstance: undefined,
-  // isAuthenticated: false,
-  idInstance: "1101820955",
-  apiTokenInstance: "cc7850ea1dd64c5ba26ce33e4ae5a2a789f512a1fd334561bf",
+const initialState: IAuthState = {
   isAuthenticated: true,
 };
-
-// export const auth = createAsyncThunk(
-//   "GET_CHARACTERS",
-//   async (payload: ICharactersRequest = {}) => {
-//     try {
-//       const response = await api.characters.fetchCharacters(payload);
-
-//       return response;
-//     } catch {
-//       throw new Error("mock");
-//     }
-//   }
-// );
 
 const slice = createSlice({
   name: "auth",
@@ -40,18 +17,11 @@ const slice = createSlice({
     setUserData: (state, action) => {
       const { apiTokenInstance, idInstance } = action.payload;
 
-      state.apiTokenInstance = apiTokenInstance;
-      state.idInstance = idInstance;
       state.isAuthenticated = true;
 
       window.apiTokenInstance = apiTokenInstance;
       window.idInstance = idInstance;
     },
-  },
-  extraReducers(builder): void {
-    // builder.addCase(getCharacters.pending, (state) => {
-    // state.isLoading = true;
-    // });
   },
 });
 
